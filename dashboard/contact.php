47e3322043f154aeacf4400867a1b126b01b8397
agravatjaimin <agravatjaimin7@gmail.com>
2024-11-23 10:31:30 +0530

    add sesion in all files

--- dashboard/contact.php
+++ dashboard/contact.php
@@ -1,3 +1,9 @@
 <?php
-$page_content = "contact-content.php";
+session_start();
+
+// Redirect to login if session not set
+if (!isset($_SESSION['id'])) {
+    header("Location: ../index.php");
+    exit;
+}$page_content = "contact-content.php";
 include "dashboard.php";
--- dashboard/recent-activities.php
+++ dashboard/recent-activities.php
@@ -1,3 +1,10 @@
 <?php
+session_start();
+
+// Redirect to login if session not set
+if (!isset($_SESSION['id'])) {
+    header("Location: ../index.php");
+    exit;
+}
 $page_content = "recent-activities-content.php";
 include "dashboard.php";
